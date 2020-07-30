553c207ab7aa1c5a465d79bc34a9543e6ea62b22
FredC <56397993+fredderf121@users.noreply.github.com>
2020-07-30 12:37:02 -0400

    Header Fixes and home page additions

--- src/js/page_functionality/header_footer.js
+++ src/js/page_functionality/header_footer.js
@@ -2,10 +2,20 @@ var scale = 'scale(1)';
 document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
 document.body.style.msTransform =   scale;       // IE 9
 document.body.style.transform = scale;
+
 $(function () {
     $("#header").load("/pages/header.html");
     $("#footer").load("/pages/footer.html");
 });
+
+
+$(window).on('load', function () {
+    console.log("hi");
+    document.getElementById("main-info").style.marginTop = "56px"; //
+});
+
+
+
 
 
 
