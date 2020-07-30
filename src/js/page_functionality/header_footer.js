436fe813b6561ab2cee980a2288ed7162d7db3ac
FredC <56397993+fredderf121@users.noreply.github.com>
2020-07-29 20:41:50 -0400

    Front page images + navigation for mobile

--- src/js/page_functionality/header_footer.js
+++ src/js/page_functionality/header_footer.js
@@ -1,3 +1,7 @@
+var scale = 'scale(1)';
+document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+document.body.style.msTransform =   scale;       // IE 9
+document.body.style.transform = scale;
 $(function () {
     $("#header").load("/pages/header.html");
     $("#footer").load("/pages/footer.html");
--- src/js/ui_animations/slideshow_animate.js
+++ src/js/ui_animations/slideshow_animate.js
@@ -1,18 +1,34 @@
 var slides = document.getElementsByClassName("slide")
 var slideIndex = 1;
 plusSlides(0);
-
+automatedSlides();
 function plusSlides(n) {
-    console.log("hi");
+
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex += n;
+    if (slideIndex > slides.length){
+        slideIndex = 1;
+    }else if (slideIndex < 1){
+        slideIndex = slides.length;
+    }
+
+    slides[slideIndex -1].style.display = "block";
+}
+
+function automatedSlides() {
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
     if (slideIndex > slides.length){
         slideIndex = 1
     }else if (slideIndex < 1){
         slideIndex = slides.length;
     }
 
+
     slides[slideIndex -1].style.display = "block";
+    setTimeout(automatedSlides, 3600); // Change image every 2 seconds
 }
