246a1c988ccc7e95e5adbb3ce80e40d033ba5fbb
haonanq666 <2648780576@qq.com>
2020-07-31 04:25:34 -0400

    finished mcv4u pages (except for math symbols, way too late to go and fix the equations now). added in the resources as links. have classes for course help page elements so they can be easily worked with.

---
+++ addfiles.py
@@ -0,0 +1,15 @@
+# this is a helper file to aid in quickly adding resources
+from os import listdir
+from os.path import isfile, join
+
+path = "/src/assets/MCV4U/Unit 8"
+onlyfiles = [f for f in listdir("."+path) if isfile(join("."+path, f))]
+
+print("<h2 class=\"coursesubheading\">Resources:</h2>\n<ul id=\"resources\">")
+for file in sorted(onlyfiles):
+
+    print("<li class=\"resourcelink\"><a target=\"_blank\" href=\""+path+"/"+file+"\">"+file+"</a></li>")
+
+
+print("</ul>")
+
