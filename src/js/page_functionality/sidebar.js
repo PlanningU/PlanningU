7f0f2cfe432a6490496721a07072d8789fd0bb2b
FredC <56397993+fredderf121@users.noreply.github.com>
2020-07-30 23:48:36 -0400

    Added sidebar mobile support

--- src/js/page_functionality/sidebar.js
+++ src/js/page_functionality/sidebar.js
@@ -1,4 +1,12 @@
-
 $(function () {
     $("#sidebar-div").load("/pages/subjects/subjects_sidebar.html");
 });
+
+function expandSideBar() {
+    var x = document.getElementById("sidebar");
+    if (x.className === "sidebar-nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "sidebar-nav";
+    }
+}
