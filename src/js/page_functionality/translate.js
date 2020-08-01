af53350d64aa1c4c72627b1d6d64bb243f9f4f10
FredC <56397993+fredderf121@users.noreply.github.com>
2020-08-01 12:47:06 -0400

    Language Toggle UI Change

--- src/js/page_functionality/translate.js
+++ src/js/page_functionality/translate.js
@@ -3,9 +3,19 @@ import Translator from "./translator.js";
 var translator = new Translator();
 
 
-$('#header').on('click', 'button.dropdown-item.btn.btn-lang.language-toggle', function () {
+/*$('#header').on('click', 'button.dropdown-item.btn.btn-lang.language-toggle', function () {
   //  console.log($(this).attr('id'));
     translator.load($(this).attr('id'));
 
-});
+});*/
 
+$('#header').on('click', 'input', function () {
+   console.log('hi');
+    if ($('input').prop('checked')===true) {
+        translator.load("zh");
+    }
+    else {
+        translator.load("en");
+    }
+
+});
