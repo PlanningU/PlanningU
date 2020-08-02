cca298e927531be81c643de9b03bbeecbd3289e5
haonanq666 <2648780576@qq.com>
2020-08-02 02:33:47 -0400

    added more pages. created contact form.

---
+++ src/js/page_functionality/contact_trans.js
@@ -0,0 +1,11 @@
+var form = document.getElementById('contact-form');
+
+$('#header').on('click', 'input', function () {
+
+    if ($('input').prop('checked')===true) {
+        form.src="https://docs.google.com/forms/d/e/1FAIpQLSd1aw-wwrCCtB2U_lXXyKWf-dwnH9F_ykNN2ceZaFAJDQeVNg/viewform?embedded=true";
+    }
+    else {
+        form.src="https://docs.google.com/forms/d/e/1FAIpQLSfwScvZ1BXj5xnUogc-uRMRYzPED4OTQmml3YqgGCBNpsxYFw/viewform?embedded=true"    }
+
+});
