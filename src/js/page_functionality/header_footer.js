98301224ec2b27b671bb883378bbef62b64d7570
FredC <56397993+fredderf121@users.noreply.github.com>
2020-08-06 14:54:02 -0400

    Language toggle saves preferences; auto-detect browser language

--- src/js/page_functionality/header_footer.js
+++ src/js/page_functionality/header_footer.js
@@ -3,6 +3,8 @@ loadHeaderFooter();
 function loadHeaderFooter() {
     $("#header").load("/pages/header.html");
     $("#footer").load("/pages/footer.html");
+
+    $("body").append('<script type="text/javascript" src="/src/js/page_functionality/cookie.js"></script>');
 }
 
 
--- src/js/page_functionality/translate.js
+++ src/js/page_functionality/translate.js
@@ -8,13 +8,37 @@ var translator = new Translator();
 
 });*/
 
+
+
+
+if (getCookie("preferred_language") === '') {
+   for (var i in navigator.languages){
+
+       if (navigator.languages[i].substring(0,2)==="zh"){
+           translator.load("zh");
+           document.getElementById("language-toggle-input").click();
+           break;
+       }
+   }
+}else{
+    translator.load(getCookie("preferred_language"));
+    if (getCookie("preferred_language")==="zh"){
+        document.getElementById("language-toggle-input").checked = true;
+
+    }else{
+        document.getElementById("language-toggle-input").checked = false;
+    }
+}
+
 $('#header').on('click', 'input', function () {
 
     if ($('input').prop('checked')===true) {
         translator.load("zh");
+        setCookie("preferred_language", "zh", 100)
     }
     else {
         translator.load("en");
+        setCookie("preferred_language", "en", 100)
     }
 
 });
