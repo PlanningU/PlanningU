888815449d4b9bc364d71b539c34dd2812a4af36
FredC <56397993+fredderf121@users.noreply.github.com>
2020-08-07 12:22:44 -0400

    Added Navigation Translations

--- src/js/page_functionality/header_footer.js
+++ src/js/page_functionality/header_footer.js
@@ -5,6 +5,9 @@ function loadHeaderFooter() {
     $("#footer").load("/pages/footer.html");
 
     $("body").append('<script type="text/javascript" src="/src/js/page_functionality/cookie.js"></script>');
+    $("body").append('<script src="/src/js/papaparse.min.js"></script> ');
+    $("body").append('<script type="module" src="/src/js/page_functionality/translate.js"></script> ');
+
 }
 
 
