e7af1ee323bbbd68f66440d1a64caca6d5680c5e
haonanq666 <2648780576@qq.com>
2020-08-08 03:24:56 -0400

    added adv functions

--- addfiles.py
+++ addfiles.py
@@ -2,12 +2,13 @@
 from os import listdir
 from os.path import isfile, join
 
-path = "/src/assets/BBB4M/"
+path = "/src/assets/MHF4U/Unit 9"
 onlyfiles = [f for f in listdir("."+path) if isfile(join("."+path, f))]
 
 print("<h2 class=\"coursesubheading\">Resources:</h2>\n<ul id=\"resources\">")
 for file in sorted(onlyfiles):
-
+    if(file.lower() == "chapter tips.docx"):
+        continue
     print("<li class=\"resourcelink\"><a target=\"_blank\" href=\""+path+"/"+file+"\">"+file+"</a></li>")
 
 
