f56b83d98dbeb689cd9ff7a3bf63f25dc019745f
FredC <56397993+fredderf121@users.noreply.github.com>
2020-08-09 23:43:52 -0400

    About Page Finishing Touches

---
+++ src/js/ui_animations/profile_hover_mobile.js
@@ -0,0 +1,17 @@
+
+$('#profiles').on('click', '.profile-card', function () {
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        if (!$(this).hasClass("expanded")){
+            jQuery(this).children(".profile-overlay").height(
+                $(this).height() - parseInt($(this).css("margin-top")));
+            jQuery(this).find(".mobile-open-arrow").toggleClass('flip');
+            $(this).addClass("expanded");
+        }else{
+            jQuery(this).children(".profile-overlay").height(
+                110 + "px");
+            $(this).removeClass("expanded");
+            jQuery(this).find(".mobile-open-arrow").toggleClass('flip');
+        }
+
+    }
+});
