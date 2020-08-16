fa9f69858dc83177b0400ce21d8b50bf6f10a90b
FredC <56397993+fredderf121@users.noreply.github.com>
2020-08-15 22:16:57 -0400

    Reworked dynamic content additions (reduced the num. of files required), updated sabrina's picture

--- src/js/page_functionality/subjects_sidebar.js
+++ src/js/page_functionality/subjects_sidebar.js
@@ -1,17 +1,3 @@
-loadSideBar();
-
-function loadSideBar() {
-    $("#sidebar-div").load("/pages/subjects/subjects_sidebar.html");
-    var x = document.getElementById("header");
-    x.className += "alongside-sidebar";
-
-    x = document.getElementById("main-info");
-    x.className += " alongside-sidebar";
-
-
-
-}
-
 $(document).ready(function () {
     try {
         document.getElementById(getCookie('current_course')).className += " show";
@@ -28,15 +14,6 @@ $('#sidebar-div').on('click', '.sidebar-item ul li a', function () {
 
 });
 
-$('#header').on('click', 'a', function () {
-    try {
-        deleteCookie("current_course");
-    } catch (typeError) {
-        void (0);
-    }
-
-})
-
 $('#sidebar-div').on('click', '.sidebar-item > a', function () {
     try {
         deleteCookie("current_course");
@@ -45,10 +22,14 @@ $('#sidebar-div').on('click', '.sidebar-item > a', function () {
     }
 });
 
+$('#header').on('click', 'a', function () {
+    try {
+        deleteCookie("current_course");
+    } catch (typeError) {
+        void (0);
+    }
 
-window.onload = function () {
-
-};
+})
 
 
 function expandSideBar() {
