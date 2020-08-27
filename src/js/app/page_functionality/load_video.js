7a454b5900d32537171e9c9710b95146cee41d5a
FredC <56397993+fredderf121@users.noreply.github.com>
2020-08-27 19:53:56 -0400

    Updated layout of home page videos and added new videos

--- src/js/app/page_functionality/load_video.js
+++ src/js/app/page_functionality/load_video.js
@@ -1,87 +1,79 @@
 //Creates the elements on the video player page
-define(function(require){
+define(function (require) {
     var parse_json = require("app/page_functionality/parse_file");
     var Plyr = require("Plyr");
     var displayArticles = require("app/page_functionality/create_cards");
 
-    return{
-        loadContent: function(){
+    return {
+        loadContent: function () {
 
             //Gets url parameter to know which videoid it is
             let url_string = window.location.href;
             let url = new URL(url_string);
 
             //THIS IS TO BE RE_ENABLED
-            //let video_id = parseInt(url.searchParams.get("videoid"));
-            let video_id = 1//TEMP CODE FOR INDEX PAGE
+            let video_id = parseInt(url.searchParams.get("videoid"));
+
 
             //Get the csv file
             parse_json.parse_csv("/src/assets/articles/video_metadata.csv")
                 .then(data => {
 
                     //Dynamically sets the page meta description to the description of the video
-                   //TEMP DISABLED $('meta[name="description"]').attr("content", data[video_id][1]);
+                    $('meta[name="description"]').attr("content", data[video_id][1]);
                     //Sets the page title to be the title of the video
-                   //TEMP DISABLED document.title = data[video_id][0];
+                    document.title = data[video_id][0];
 
 
                     //Creating the video element and its children
-                    let video = document.getElementById('video-player');
-                    //Temp video.setAttribute("data-poster", data[video_id][3]);
+                    let video = document.getElementsByTagName('iframe')[0];
 
-                    let source = document.createElement('source');
 
-                    //TEMP source.setAttribute('src', data[video_id][2]);
-                    source.setAttribute('type', "video/mp4");
+                    video.src = data[video_id][2];
 
                     //video.appendChild(source);
                     //const player = new Plyr(video, {});
-                   // window.player = player;
+                    // window.player = player;
                     //----------------------------------------
 
                     //Setting descriptions TODO: Make it more dynamic and not repeat the same code found in
                     // create_cards?
 
-                    var text_elements = document.querySelectorAll('.video-player.video-text');
+                    var text_elements = document.querySelectorAll('.video-player.video-text')[0];
 
-                    for(let i= 0; i<text_elements.length; i++){
-                        let sub_element;
-                        sub_element = document.createElement("h3");
-                        sub_element.className = "video-title";
-                        sub_element.innerHTML = data[i+1][0];
-                        text_elements[i].appendChild(sub_element);
+                    let sub_element;
+                    sub_element = document.createElement("h3");
+                    sub_element.className = "video-title";
+                    sub_element.innerHTML = data[video_id][0];
+                    text_elements.appendChild(sub_element);
 
-                        //VIDEO TAGS DIV
-                        sub_element = document.createElement("div");
-                        sub_element.className = "tags";
-                        let sub_sub_element = text_elements[i].appendChild(sub_element);
+                    //VIDEO TAGS DIV
+                    sub_element = document.createElement("div");
+                    sub_element.className = "tags";
+                    let sub_sub_element = text_elements.appendChild(sub_element);
 
-                        //VIDEO INDIVIDUAL TAG
-                        let tags = data[i+1][4].replace(/"/g, "").split(',');
-                        for (let i = 0; i < tags.length; i++) {
-                            let tag = document.createElement("span");
-                            tag.className = "tag";
-                            tag.innerHTML = tags[i];
-                            sub_sub_element.appendChild(tag);
-
-                        }
-
-                        //VIDEO DESCRIPTION
-                        sub_element = document.createElement("p");
-                        sub_element.className = "video-description";
-                        sub_element.innerHTML = data[i+1][1];
-                        text_elements[i].appendChild(sub_element);
+                    //VIDEO INDIVIDUAL TAG
+                    let tags = data[video_id][4].replace(/"/g, "").split(',');
+                    for (let i = 0; i < tags.length; i++) {
+                        let tag = document.createElement("span");
+                        tag.className = "tag";
+                        tag.innerHTML = tags[i];
+                        sub_sub_element.appendChild(tag);
 
                     }
+
+                    //VIDEO DESCRIPTION
+                    sub_element = document.createElement("p");
+                    sub_element.className = "video-description";
+                    sub_element.innerHTML = data[video_id][1];
+                    text_elements.appendChild(sub_element);
 
 
                 });
 
 
-
         }
     }
-
 
 
 })
--- src/js/pages/page_index.js
+++ src/js/pages/page_index.js
@@ -1,16 +1,13 @@
 //THIS IS WHERE YOU INCLUDE WHAT TO RUN BEFORE TRANSLATING
 //Generally, it will be anything that contains a .load jquery function (since it likely contains text to translate)
-var modules_before_translate = [
-    'app/page_functionality/header_footer',
-    'app/page_functionality/load_video'
-];
+var modules_before_translate = ['app/page_functionality/header_footer', 'app/page_functionality/create_cards'];
 
 require(['../common'], function (common) {
 //This requires a double nested require for common and then jquery because we need to first load the common
 //file directory (i.e. baseURL, jquery: path, etc.), before we can directly access jquery
     require(['jquery'], function ($) {
 
-        require(modules_before_translate.concat(['app/page_functionality/translate']), function (hf, load_video, translate) {
+        require(modules_before_translate.concat(['app/page_functionality/translate']), function (hf, create_cards, translate) {
             var d1 = new $.Deferred();
             var d2 = new $.Deferred();
             $.when(d1, d2).then(function () {
@@ -19,8 +16,9 @@ require(['../common'], function (common) {
             });
             hf.loadContent(d1, d2);
 
-            //Temp video
-            load_video.loadContent();
+            new create_cards.VideoCards(
+                "/src/assets/articles/video_metadata.csv",
+                "video-section");
 
         });
 
